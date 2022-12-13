5f1471964efe70e3ff7cf4bc12223a9326d4ac31
Elena <agapitovae87@gmail.com>
2022-12-13 21:54:43 +0300

    Change HW1_1

--- HomeWork/HW1/HW1_1.py
+++ HomeWork/HW1/HW1_1.py
@@ -4,16 +4,7 @@
 day_number = int(input('Введите число, обозначающую день недели (от 1 до 7 включительно): '))
 if day_number == 6 or day_number == 7:
     print(f'День недели {day_number} -> выходной день')
-else:
+elif 1 <= day_number <= 5:
     print(f'День недели {day_number} -> рабочий день')
-
-
-
-
-
-
-
-
-
-
-
+else:
+    print('Введено значение вне заданного диапозона')
