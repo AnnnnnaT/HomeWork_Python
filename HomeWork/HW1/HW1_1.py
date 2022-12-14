7c04a6e04a129b5da8dc8a3d343e90449646488a
Elena <agapitovae87@gmail.com>
2022-12-14 12:45:44 +0300

    Add code create list-snake

--- HomeWork/HW1/HW1_1.py
+++ HomeWork/HW1/HW1_1.py
@@ -1,10 +1,34 @@
-# Задача 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и
-# проверяет, является ли этот день выходным.
+# # Задача 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и
+# # проверяет, является ли этот день выходным.
 
-day_number = int(input('Введите число, обозначающую день недели (от 1 до 7 включительно): '))
-if day_number == 6 or day_number == 7:
-    print(f'День недели {day_number} -> выходной день')
-elif 1 <= day_number <= 5:
-    print(f'День недели {day_number} -> рабочий день')
-else:
-    print('Введено значение вне заданного диапозона')
+# day_number = int(input('Введите число, обозначающую день недели (от 1 до 7 включительно): '))
+# if day_number == 6 or day_number == 7:
+#     print(f'День недели {day_number} -> выходной день')
+# elif 1 <= day_number <= 5:
+#     print(f'День недели {day_number} -> рабочий день')
+# else:
+#     print('Введено значение вне заданного диапозона')
+
+# Заполнить список змейкой
+n = int(input())
+m = int(input())
+value = 0
+mas = []
+for i in range(n):
+    mas_str = []
+    if i%2==0: 
+        for j in range(m):
+            mas_str.append(value)
+            value+=1
+        mas.append(mas_str)
+    else:
+        for j in range(m):
+            mas_str.append(value)
+            value+=1
+        mas_str.reverse()    
+        mas.append(mas_str)             
+
+for i in range(n):
+    for j in range(m):
+        print(mas[i][j], end=' ')
+    print()   
