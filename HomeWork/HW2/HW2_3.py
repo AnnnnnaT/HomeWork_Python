f3c8be0e5e2dab50e5321305684884c526105805
Elena <agapitovae87@gmail.com>
2022-12-16 19:15:57 +0300

    Change HW2_3

--- HomeWork/HW2/HW2_3.py
+++ HomeWork/HW2/HW2_3.py
@@ -1,19 +1,22 @@
 # Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE,
 # максимум использование библиотеки Random для получения случайного int
 
-import random
-# Создаем список рандомно:
+from random import randint
+
+# 1. Создаем список рандомно:
 # n = int(input('Введите количество элементов списка: '))
 # value_min = int(input('Введите минимальный элемент списка: '))
 # value_max = int(input('Введите максимальный элемент списка: '))
-# first_list = [random.randint(value_min, value_max) for _ in range(n)]
+# first_list = [randint(value_min, value_max) for _ in range(n)]
 
-# Создаем упорядоченный список двузначных элементов (другой способ решения)
+# 2. Создаем упорядоченный список двузначных элементов (другой способ решения)
 # подобное делала на С#
-first_list = list(range(10, 100))
+# first_list = list(range(10, 100))
+# 3. И еще один вариант, долой ограничения))))
+first_list = [randint(1, 100) for _ in range(5, 20)]
 second_list = first_list.copy()
 for i in range(len(second_list)):
-    j = random.randint(0, (len(second_list) - 1))
+    j = randint(0, (len(second_list) - 1))
     if i != j:
         second_list[i], second_list[j] = second_list[j], second_list[i]
 print(f'Созданный список: {first_list}')
